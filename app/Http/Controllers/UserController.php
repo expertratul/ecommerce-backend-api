5b4ab54a8698e2df6f95a1682580b25d3fab5fb8
expertratul <seoratrul24@gmail.com>
2025-11-27 14:38:54 +0600

    UserController Up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,8 +12,6 @@ class UserController extends Controller
     public function register(Request $request)
     {
         // Registration logic here
-        // bdhfbvdfvdfhfvbdfvdf vdfbvhj
-        // bvfhdjbvdfjhvdfvdhjvbdfv
     }
 
     /**
