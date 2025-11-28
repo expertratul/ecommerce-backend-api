398aff074988df929f22c65256c5c085f446df8c
expertratul <seoratrul24@gmail.com>
2025-11-28 10:48:05 +0600

    Database Migration Complete

---
+++ database/migrations/2025_08_05_154526_create_invoices_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            
+            $table->decimal('total', 10, 2);
+            $table->decimal('vat', 10, 2);
+            $table->decimal('payable', 10, 2);
+            $table->string('cus_details',500);
+            $table->string('ship_details',500);
+            $table->string('tran_id',100);
+            $table->string('val_id',100)->default(0);
+            $table->enum('delivery_status',['Pending','Processing','Completed']);
+            $table->string('payment_status');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->cascadeOnUpdate()->restrictOnDelete();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
